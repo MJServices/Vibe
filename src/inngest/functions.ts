9af118ed02518c863f3b3c9adafe765dcde19288
MJ <mjservices410@gmail.com>
2025-10-05 13:41:43 +0500

    06: e2b Sandboxes

--- src/inngest/functions.ts
+++ src/inngest/functions.ts
@@ -1,20 +1,33 @@
 import { inngest } from "./client";
 import { Agent, gemini, createAgent } from "@inngest/agent-kit";
+import SandBox from "@e2b/code-interpreter";
+import { getSandboxUrl } from "./utils";
 
 export const helloWorld = inngest.createFunction(
   { id: "hello-world" },
   { event: "test/hello.world" },
   async ({ event, step }) => {
+    const sandboxId = await step.run("get-sandbox-id", async () => {
+      const sandbox = await SandBox.create("vibe-nextjs-vibe2");
+      return sandbox.sandboxId;
+    });
+
     const code_agent = createAgent({
       name: "code_agent",
-      system: "you are an expert next js developer, you write readable and maintainable write simple Nextjs, and React snippets",
+      system:
+        "you are an expert next js developer, you write readable and maintainable write simple Nextjs, and React snippets",
       model: gemini({ model: "gemini-2.5-pro" }),
     });
-    const {output} = await code_agent.run(
+    const { output } = await code_agent.run(
       `Write the following snippets: ${event.data.value}`
     );
-    console.log("output", output[0]);
 
-    return { output };
+    const sandboxUrl = await step.run("get-sandbox-url", async () => {
+      const sandbox = await getSandboxUrl(sandboxId);
+      const host = sandbox.getHost(3000);
+      return `https://${host}`;
+    });
+
+    return { output, sandboxUrl };
   }
 );
